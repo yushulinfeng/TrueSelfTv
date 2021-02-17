67d86bf80acb59c09c127436498a9ce4ee466314
sunyulin01 <sunyulin01@baidu.com>
2021-02-17 19:55:34 +0800

    增加设备信息获取

--- app/src/main/java/org/out/yslf/trueselftv/MainActivity.java
+++ app/src/main/java/org/out/yslf/trueselftv/MainActivity.java
@@ -1,17 +1,26 @@
 package org.out.yslf.trueselftv;
 
 import android.app.Activity;
+import android.app.ActivityManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StatFs;
+import android.text.format.Formatter;
+import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Switch;
 
 import org.out.yslf.trueselftv.utils.EasyDialogBuilder;
+import org.out.yslf.trueselftv.utils.FileShowTool;
 import org.out.yslf.trueselftv.utils.LockScreenTool;
 import org.out.yslf.trueselftv.utils.PermissionsTools;
 import org.out.yslf.trueselftv.utils.ShareTool;
 import org.out.yslf.trueselftv.utils.ToastTool;
+
+import java.io.File;
 
 public class MainActivity extends Activity {
 
@@ -89,6 +98,60 @@ public class MainActivity extends Activity {
     public void onWriteLaunchLogClick(View view) {
         ShareTool.writeLaunchTimeLog(this);
         ToastTool.showToast(this, "已写入测试的开机时间");
+    }
+
+    public void onShowSystemInfoClick(View view) {
+        // 获取内存信息
+        ActivityManager mActivityManager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+        ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
+        mActivityManager.getMemoryInfo(memoryInfo);
+        long memSize = memoryInfo.totalMem;
+        long availMem = memoryInfo.availMem;
+
+        // 获取磁盘信息
+        File path = Environment.getExternalStorageDirectory();
+        StatFs stat = new StatFs(path.getPath());
+        long blockSize = stat.getBlockSize();
+        long totalBlocks = stat.getBlockCount();
+        String totalSdSize = Formatter.formatFileSize(this, blockSize * totalBlocks);
+        long availableBlocks = stat.getAvailableBlocks();
+        String availableSdSize = Formatter.formatFileSize(this, blockSize * availableBlocks);
+
+        // 屏幕宽高
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        String screenSize = "屏幕尺寸：" + metrics.widthPixels + "x" + metrics.heightPixels + "\n"
+                + "像素密度：" + metrics.density;
+
+        // 显示对话框
+        String tip = "内存(可用/全部)：" + FileShowTool.formatFileSize(availMem)
+                + "/" + FileShowTool.formatFileSize(memSize) + "\n"
+                + "磁盘(可用/全部)：" + availableSdSize + "/" + totalSdSize + "\n"
+                + screenSize + "\n"
+                + "BOARD:" + Build.BOARD + "\n"
+                + "BRAND:" + Build.BRAND + "\n"
+                + "CPU_ABI:" + Build.CPU_ABI + "\n"
+                + "CPU_ABI2:" + Build.CPU_ABI2 + "\n"
+                + "DEVICE:" + Build.DEVICE + "\n"
+                + "DISPLAY:" + Build.DISPLAY + "\n"
+                + "FINGERPRINT:" + Build.FINGERPRINT + "\n"
+                + "HARDWARE:" + Build.HARDWARE + "\n"
+                + "HOST:" + Build.HOST + "\n"
+                + "ID:" + Build.ID + "\n"
+                + "MANUFACTURER:" + Build.MANUFACTURER + "\n"
+                + "MODEL:" + Build.MODEL + "\n"
+                + "PRODUCT:" + Build.PRODUCT + "\n"
+                + "TAGS:" + Build.TAGS + "\n"
+                + "TIME:" + Build.TIME + "\n"
+                + "TYPE:" + Build.TYPE + "\n"
+                + "USER:" + Build.USER + "\n"
+                + "VERSION.CODENAME:" + Build.VERSION.CODENAME + "\n"
+                + "VERSION.RELEASE:" + Build.VERSION.RELEASE + "\n"
+                + "VERSION.SDK_INT:" + Build.VERSION.SDK_INT;
+        EasyDialogBuilder.builder(this)
+                .setTitle("系统信息")
+                .setMessage(tip)
+                .setPositiveButton("确定", null)
+                .show();
     }
 
     public void onLaunchLogClick(View view) { // 查看开机记录
--- app/src/main/java/org/out/yslf/trueselftv/utils/FileShowTool.java
+++ app/src/main/java/org/out/yslf/trueselftv/utils/FileShowTool.java
@@ -14,6 +14,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -213,5 +214,24 @@ public class FileShowTool {
             e.printStackTrace();
             ToastTool.showToast(context, "打开文件失败");
         }
+    }
+
+    public static String formatFileSize(long fileS) {
+        DecimalFormat df = new DecimalFormat("#.00");
+        String fileSizeString = "";
+        String wrongSize = "0B";
+        if (fileS == 0) {
+            return wrongSize;
+        }
+        if (fileS < 1024) {
+            fileSizeString = df.format((double) fileS) + "B";
+        } else if (fileS < 1048576) {
+            fileSizeString = df.format((double) fileS / 1024) + "KB";
+        } else if (fileS < 1073741824) {
+            fileSizeString = df.format((double) fileS / 1048576) + "MB";
+        } else {
+            fileSizeString = df.format((double) fileS / 1073741824) + "GB";
+        }
+        return fileSizeString;
     }
 }
